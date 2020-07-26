5a86f6952c52574984f33ad02c59d672aeb7dae5
Natalia Teaca <lilikoy@sbcglobal.net>
2020-07-26 11:39:39 -0700

    Add Heroku

--- server.js
+++ server.js
@@ -43,7 +43,10 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
-
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 app.get('/api/animals', (req, res) => {
     let results = animals;
    if (req.query) {
@@ -53,6 +56,18 @@ app.get('/api/animals', (req, res) => {
    
   });
 
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+      res.json(result);
+  });
+
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
+
+ 
